e6768d2c5a2ac82e8663a98f15f124a26e20dcdc
David AW Barton <david.barton@bristol.ac.uk>
2020-11-25 22:29:11 +0000

    WIP: NTMD_MSSP_20

--- src/NonlinearSciMLData.jl
+++ src/NonlinearSciMLData.jl
@@ -7,7 +7,7 @@ nonlinear systems.
 module NonlinearSciMLData
 
 # Functions
-export paper_doi, data_doi, describe, data_dir
+export paper_doi, data_doi, describe, data_path
 
 # Datasets
 export NTMD_MSSP_2017
@@ -38,12 +38,28 @@ Return a short description of the dataset.
 function describe end
 
 """
-    data_dir(dataset)
+    experiments(dataset)
+
+Return a `Dict{Symbol,String}` of experiments within the dataset.
+"""
+function experiments end
+
+"""
+    experiment(dataset, label)
+
+Return an array of measured inputs and outputs for a particular experiment. The array might
+contain nested arrays to indicate where observations have been made under the same
+conditions.
+"""
+function experiment end
+
+"""
+    data_path(dataset)
 
 Return the directory containing the dataset.
 """
-function data_dir(dataset)
-    datadep = "NonlinearSciML."*string(nameof(dataset))
+function data_path(dataset)
+    datadep = string(@__MODULE__) * "." * string(nameof(dataset))
     return DataDeps.@datadep_str datadep
 end
 
@@ -58,6 +74,13 @@ function init_data()
     end
 end
 
+"""
+    register_data(dataset)
+
+Register a particular data dependency.
+"""
+function register_data end
+
 # Package initialisation
 function __init__()
     return init_data()
@@ -65,7 +88,14 @@ end
 
 # Actual datasets
 include("datasets/NTMD_MSSP_2017.jl")
+using ._NTMD_MSSP_2017
 
 const DATASETS = [NTMD_MSSP_2017]
 
 end
+
+# Other datasets
+# https://data.bris.ac.uk/data/dataset/15xkehnayh6st11gbc9dmeqg6v
+# https://data.bris.ac.uk/data/dataset/1pfqddbxy8ry91n8j5c0cbeqxe
+# https://data.bris.ac.uk/data/dataset/1jlp5gsztp3xr2hngpf0q72uii
+# https://data.bris.ac.uk/data/dataset/1ttza2mrigjbt2mmbp6raqrxen
--- src/datasets/NTMD_MSSP_2017.jl
+++ src/datasets/NTMD_MSSP_2017.jl
@@ -1,22 +1,84 @@
-# Control-based continuation: Bifurcation and stability analysis for physical experiments
-# David A.W. Barton
-# Mechanical Systems and Signal Processing, Volume 84, Part B, 1 February 2017, Pages 54-64
+"""
+Control-based continuation: Bifurcation and stability analysis for physical experiments
+David A.W. Barton
+Mechanical Systems and Signal Processing, Volume 84, Part B, 1 February 2017, Pages 54-64
+"""
+module _NTMD_MSSP_2017
+
+export NTMD_MSSP_2017
+
+using ..NonlinearSciMLData: NonlinearSciMLData, describe
+using DataDeps: register, DataDep, unpack
 
 struct NTMD_MSSP_2017 end
 
-paper_doi(::Type{NTMD_MSSP_2017}) = "https://doi.org/10.1016/j.ymssp.2015.12.039"
-data_doi(::Type{NTMD_MSSP_2017}) = "https://doi.org/10.5523/bris.1esk6h8klq0bg1gdcr78a2ypyz"
+function NonlinearSciMLData.paper_doi(::Type{NTMD_MSSP_2017})
+    return "https://doi.org/10.1016/j.ymssp.2015.12.039"
+end
+function NonlinearSciMLData.data_doi(::Type{NTMD_MSSP_2017})
+    return "https://doi.org/10.5523/bris.1esk6h8klq0bg1gdcr78a2ypyz"
+end
 
-function describe(::Type{NTMD_MSSP_2017})
+function NonlinearSciMLData.describe(::Type{NTMD_MSSP_2017})
     return "Experimental data associated with the paper \"Control-based continuation: bifurcation and stability analysis for physical experiments\" by David A.W. Barton"
 end
 
-function register_data(::Type{NTMD_MSSP_2017})
+function NonlinearSciMLData.experiments(::Type{NTMD_MSSP_2017})
+    return Dict(
+        :E1ai =>
+            "Stable orbit. Uncontrolled with random inputs. Variety of random amplitudes [0.1:0.1:0.5] x 20 sec x 10 replicates.",
+        :E1aii =>
+            "Stable orbit. Uncontrolled with random inputs. A long time capture (2 minutes) x2.",
+        :E1bi =>
+            "Stable orbit. Controlled with random inputs. Variety of random amplitudes [0.1:0.1:0.5] x 20 sec x 10 replicates.",
+        :E1bii =>
+            "Stable orbit. Controlled with random inputs. A long time capture (2 minutes) x2.",
+        :E2bi =>
+            "Unstable orbit. Controlled with random inputs. Variety of random amplitudes [0.1:0.1:0.5] x 20 sec x 10 replicates.",
+        :E2bii =>
+            "Unstable orbit. Controlled with random inputs. A long time capture (2 minutes) x2.",
+        :E2ci =>
+            "Unstable orbit. Uncontrolled release followed by return to control. 40 replicates.",
+        :E3a =>
+            "Continuation of a single branch. Variety of random amplitudes [0.1, 0.3, 0.5] x 20 sec.",
+        :E3b =>
+            "Continuation of a single branch. Several replicates for fixed random amplitude (0.5).",
+        :E4 => "Continuation of the full surface.",
+    )
+end
+
+function get_meta(data)
+    timestamp = DateTime(floor.(Int, data["timestamp_created"])...)
+    fourier = data["fourier"]
+end
+
+function get_observations(data)
+    # base is the input
+    # x is the output
+
+    # unfortunately it would have been better to have an accelerometer on the base rather than a displacement sensor...
+end
+
+function NonlinearSciMLData.experiment(::Type{NTMD_MSSP_2017}, label::Symbol)
+    if label === :E1ai
+    elseif label === :E1aii
+    elseif label === :E1bi
+    elseif label === :E1bii
+    elseif label === :E2bi
+    elseif label === :E2bii
+    elseif label === :E2ci
+    elseif label === :E2cii
+    end
+end
+
+function NonlinearSciMLData.register_data(::Type{NTMD_MSSP_2017})
     return register(DataDep(
-        "NonlinearSciMLData.NTMD_MSSP_2017",
+        string(nameof(NonlinearSciMLData)) * "." * string(nameof(NTMD_MSSP_2017)),
         describe(NTMD_MSSP_2017),
         "http://data.bris.ac.uk/datasets/1esk6h8klq0bg1gdcr78a2ypyz/2015_MSSP_Barton.zip",
         "711bb90e42c4cdbf3c98829108b84489e39801f567868d6096a24361672a40f5";
         post_fetch_method=unpack,
     ))
+end
+
 end
